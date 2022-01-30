6c0aacc92473812e669fc3ca50e0abc8f09ad449
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-01-30 14:36:14 +0200

    add task2

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# goit-react-hw-01-components
-
-goit-react-hw-01-components
--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import styles from "./app.module.css";
 import SocialProfile from "../components/SocialProfile/SocialProfile";
 import user from "../data/user.json";
+import data from "../data/data.json";
+import Statistics from "../components/Statistics/Statistics";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 }
